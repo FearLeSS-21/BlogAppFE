0118f500859f455a25759b38fcd79d9b3ec0ba4a
Zeyad Wael <Zeyad.wael.2003@gmail.com>
2024-09-23 13:16:52 +0300

    Main Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component'; // Import HeaderComponent
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [
-    RouterOutlet, // Include RouterOutlet if routing is used
-    HeaderComponent // Include HeaderComponent
-  ]
+  imports: [ ]
 })
 export class AppComponent {
   title = 'my-angular-app';
